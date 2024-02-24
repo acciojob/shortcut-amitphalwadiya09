66e44f9198001fafb69e73714a26c136ed64f336
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-02-24 11:01:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 function shortcut(s1, s2) {
-  // your code here
-	if(s1.length>0 && s2.length>0)
-	{
-		let ans=s1[0].toUpperCase()+s2[0].toUpperCase()
-		return ans;
-	}
-	else
-	{
-		return '';
-	}
+if (s1.length > 0 && s2.length > 0) {
+        const initialLetters = s1[0].toUpperCase() + s2[0].toLowerCase();
+        return initialLetters;
+    } else {
+        return '';
+    }
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(shortcut(s1, s2));
